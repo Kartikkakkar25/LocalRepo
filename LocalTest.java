81882a5c22e9ab7d56c30ae464914cde181c6109
Kartikkakkar25 <kartik.kakkar190@gmail.com>
2024-09-14 20:32:04 +0530

    added a constructor

--- LocalTest.java
+++ LocalTest.java
@@ -3,7 +3,7 @@ package LocalRepo;
 public class LocalTest {
     public static void main(String[] args) {
         System.out.println("This is a new Repo");
-        T1 t = new T1();
+        T1 t = new T1(6);
         t.display();
     }
 }
